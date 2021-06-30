90dc09b922939cf9bb857394ff14fe7369d52a41
unknown <nikhil.intquo@gmail.com>
2021-06-30 12:27:34 +0530

    first commit

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Employee;
+use PDF;
+
+class EmployeeController extends Controller
+{
+
+     // Display user data in view
+    public function showEmployees(){
+        $employee = Employee::all();
+        return view("employee",compact("employee"));
+    }
+
+    //Generate PDF
+    public function createPDF(){
+        // retreive all records from db
+        $data = Employee::all();
+
+        // share data to view
+        view()->share('employee',$data);
+        $pdf = PDF::loadView('employee', $data);
+
+         // download PDF file with download method
+        return $pdf->download('pdf_file.pdf');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
